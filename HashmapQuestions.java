067e89b95885dc4a093d47354f638f029462256c
Gursahib Singh <gursahib99888@gmail.com>
2023-07-11 04:59:18 -0700

    11 July, 2023-2

---
+++ HashmapQuestions.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class HashmapQuestions {
+    public static void majorElement(int nums[]){
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])){
+                map.put(nums[i], map.get(nums[i])+1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        for (int key : map.keySet()) {
+            if (map.get(key) > nums.length/3){
+                System.out.println(key);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int nums[] = {1, 3, 2, 5, 1, 3, 1, 5, 1};
+        majorElement(nums);
+    }
+}
